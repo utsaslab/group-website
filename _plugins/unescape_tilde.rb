55a2022c913f6cf6e72eca27050bcc78d3d6d06c
Vijay Chidambaram <vijayc@utexas.edu>
2025-08-02 14:10:06 -0500

    Handle non-breaking spaces in tilde filter

--- _plugins/unescape_tilde.rb
+++ _plugins/unescape_tilde.rb
@@ -1,7 +1,11 @@
 module Jekyll
   module TildeFilter
+    NBSP = [0xA0].pack('U')
+
     def unescape_tilde(input)
-      input.to_s.gsub(/%7E/i, '~')
+      input.to_s
+           .gsub(/%7E|%C2%A0/i, '~')
+           .gsub(NBSP, '~')
     end
   end
 end
